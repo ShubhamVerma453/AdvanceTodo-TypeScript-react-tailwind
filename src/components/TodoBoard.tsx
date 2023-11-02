f9f48746c37c5a80346ea4760daa696a31215c31
ShubhamVerma453 <sv4537373@gmail.com>
2023-11-02 10:30:47 +0530

    config. button ui added

--- src/components/TodoBoard.tsx
+++ src/components/TodoBoard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from 'react-redux';
 import { selectData } from "../store/dataSlice";
 
@@ -9,6 +9,7 @@ interface SideBarProps {
 const TodoBoard: React.FC<SideBarProps> = ({ isOpen }) => {
 
     let data = useSelector(selectData)[0];
+    const [isCongigureOpen, setIsCongigureOpen] = useState(false);
     const colors = [
         "bg-red-500",
         "bg-yellow-500",
@@ -19,7 +20,11 @@ const TodoBoard: React.FC<SideBarProps> = ({ isOpen }) => {
         "bg-orange-500",
         "bg-pink-500",
         "bg-sky-500",
-      ];
+    ];
+
+    function toggleIsCongigureOpen(){
+        setIsCongigureOpen(preState => !preState);
+    }
 
     return <div className={`bg-[#f4f7fd] dark:bg-black rounded-tl-2xl p-6 mt-[84px] smooth ${isOpen && "side-bar-open"}`}>
 
@@ -27,7 +32,15 @@ const TodoBoard: React.FC<SideBarProps> = ({ isOpen }) => {
             <h1 className="text-3xl font-bold mr-4">{data.name}</h1>
             <div className="flex gap-5 items-center">
                 <div title="Add task" className="text-xl bg-slate-300 dark:bg-slate-700 cursor-pointer px-3 py-1 rounded-full hover:bg-[#03C988] dark:hover:bg-[#03C988]"><i className="fa-solid fa-plus"></i></div>
-                <div title="Configure board" className="text-xl bg-slate-300 dark:bg-slate-700 cursor-pointer px-3 py-1 rounded-full hover:bg-[#03C988] dark:hover:bg-[#03C988]"><i className="fa-solid fa-gears"></i></div>
+                <div title="Configure board" onClick={toggleIsCongigureOpen} className="text-xl relative bg-slate-300 dark:bg-slate-700 cursor-pointer px-3 py-1 rounded-full hover:bg-[#03C988] dark:hover:bg-[#03C988]">
+                    <i className="fa-solid fa-gears"></i>
+                    {isCongigureOpen &&
+                        <div className="bg-white dark:bg-[#2b2c37] shadow-sm w-32 p-2 rounded-lg text-sm absolute top-10 left-0 z-10">
+                            <div className="hover:bg-[#03C988] px-2 py-1 rounded-lg">edit board</div>
+                            <div className="text-red-600 hover:bg-red-300 px-2 py-1 rounded-lg">delete board</div>
+                        </div>
+                    }
+                </div>
             </div>
         </div>
 
@@ -38,7 +51,7 @@ const TodoBoard: React.FC<SideBarProps> = ({ isOpen }) => {
 
                     <div className="my-4 p-4 mr-5 w-80  inline-block align-top" key={index}>
                         <div className="flex items-center gap-3 mb-7">
-                            <span className={`${colors[index%(colors.length)]} inline-block w-5 h-5 rounded-full`}></span>
+                            <span className={`${colors[index % (colors.length)]} inline-block w-5 h-5 rounded-full`}></span>
                             {item.name} ({item.tasks.length})
                         </div>
 
